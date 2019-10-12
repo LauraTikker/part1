115666c691165bcf2ce42b6e6a646c51161d4a70
Laura <Laura@DESKTOP-78U8GT2>
2019-10-12 15:25:00 +0300

    first

--- practice/src/index.js
+++ practice/src/index.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    const name = 'Peter'
-    const age = 10    
-
-    console.log('Hello from component')
-
-    return ( 
-    <div>
-        <h2> Greetings</h2>  
-        <Hello name="Maya" age={26 + 10} />
-        <Hello name={name} age={age} />   
-        </div>
-    )
-}
-
-const Hello = (props) => {
-    return ( 
+const App = (props) => {
+    const [clicks, setClicks] = useState({
+      left: 0, right: 0
+    })
+  
+    const handleLeftClick = () => {
+      const newClicks = { 
+        left: clicks.left + 1, 
+        right: clicks.right 
+      }
+      setClicks(newClicks)
+    }
+  
+    const handleRightClick = () => {
+      const newClicks = { 
+        left: clicks.left, 
+        right: clicks.right + 1 
+      }
+      setClicks(newClicks)
+    }
+  
+    return (
+      <div>
         <div>
-        <p> Hello world {props.name}, you are {props.age} years old</p>  
+          {clicks.left}
+          <button onClick={handleLeftClick}>left</button>
+          <button onClick={handleRightClick}>right</button>
+          {clicks.right}
         </div>
+      </div>
     )
-}
+  }
 
 ReactDOM.render( < App /> , document.getElementById('root'))
